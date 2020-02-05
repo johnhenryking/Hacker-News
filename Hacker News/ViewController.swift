95437c17725abb7099692b83c0f4f8183c79d7d7
John King <johnhenryking@aol.com>
2020-02-05 16:52:54 -0500

    Initial Commit

---
+++ Hacker News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hacker News
+//
+//  Created by User on 2/5/20.
+//  Copyright © 2020 jonathanking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hacker NewsTests/Hacker_NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hacker_NewsTests.swift
+//  Hacker NewsTests
+//
+//  Created by User on 2/5/20.
+//  Copyright © 2020 jonathanking. All rights reserved.
+//
+
+import XCTest
+@testable import Hacker_News
+
+class Hacker_NewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
